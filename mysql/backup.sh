073e39ed5c3f5b9de3f49b7bef26966827974a6c
Harshal Sheth <git@harshal.sheth.io>
2020-11-18 05:51:24 +0000

    Add mysql backups
    Closes coursetable/coursetable#167

---
+++ mysql/backup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -euo pipefail
+
+CURRENT_DATE=$(date +"%Y-%m-%d")
+
+# Normal backups.
+# These go into db_dumps.
+# TODO: configure a retention policy. See backup-upload.sh for reference.
+if [ -z "${1-}" ]; then
+	BACKUP_TAR_GZ_FILE="$(pwd)/db_dumps/backups-${CURRENT_DATE}.tar.gz"
+else
+	# If specified, this must be an absolute path.
+	BACKUP_TAR_GZ_FILE="$1"
+fi
+
+docker-compose exec mysql sh -c 'exec mysqldump yaleplus -uroot -p"$MYSQL_ROOT_PASSWORD"' > /tmp/yaleplus.sql
+docker-compose exec mysql sh -c 'exec mysqldump yale_advanced_oci -uroot -p"$MYSQL_ROOT_PASSWORD"' > /tmp/yale_advanced_oci.sql
+
+pushd /tmp
+tar -czf "$BACKUP_TAR_GZ_FILE" yaleplus.sql yale_advanced_oci.sql
+rm -f yaleplus.sql yale_advanced_oci.sql
+popd
+
+# Dump the worksheet_courses table every day.
+WORKSHEET_DUMP_FILE="worksheets/worksheet-${CURRENT_DATE}.sql"
+docker-compose exec mysql sh -c 'exec mysqldump yale_advanced_oci worksheet_courses -uroot -p"$MYSQL_ROOT_PASSWORD"' > ${WORKSHEET_DUMP_FILE}
+
