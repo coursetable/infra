68ba47027c067e56c5fda5c159395596c5090c0a
Kevin Hu <kevinhuwest@gmail.com>
2021-04-07 23:29:43 -0700

    Update backup.sh

--- mysql/backup.sh
+++ mysql/backup.sh
@@ -15,14 +15,13 @@ else
 fi
 
 docker-compose exec -T mysql sh -c 'exec mysqldump yaleplus -uroot -p"$MYSQL_ROOT_PASSWORD"' > /tmp/yaleplus.sql
-docker-compose exec -T mysql sh -c 'exec mysqldump yale_advanced_oci -uroot -p"$MYSQL_ROOT_PASSWORD"' > /tmp/yale_advanced_oci.sql
 
 pushd /tmp > /dev/null
-tar -czf "$BACKUP_TAR_GZ_FILE" yaleplus.sql yale_advanced_oci.sql
-rm -f yaleplus.sql yale_advanced_oci.sql
+tar -czf "$BACKUP_TAR_GZ_FILE" yaleplus.sql
+rm -f yaleplus.sql
 popd > /dev/null
 
 # Dump the worksheet_courses table every day.
 WORKSHEET_DUMP_FILE="worksheets/worksheet-${CURRENT_DATE}.sql"
-docker-compose exec -T mysql sh -c 'exec mysqldump yale_advanced_oci worksheet_courses -uroot -p"$MYSQL_ROOT_PASSWORD"' > ${WORKSHEET_DUMP_FILE}
+docker-compose exec -T mysql sh -c 'exec mysqldump yaleplus WorksheetCourses -uroot -p"$MYSQL_ROOT_PASSWORD"' > ${WORKSHEET_DUMP_FILE}
 
