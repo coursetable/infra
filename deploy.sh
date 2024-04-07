d7854f967fe2b022343c0a785c8eb8e3e675539e
Neil Song <song.neil.song@gmail.com>
2024-04-07 00:08:42 -0400

    init postgres migration

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-doppler setup -p coursetable -c prd
+doppler setup -p coursetable -c prod
 (cd traefik && doppler run --command "docker-compose up -d")
 (cd under_maintenance && doppler run --command "docker-compose up -d")
 (cd mysql && doppler run --command "docker-compose up -d")
