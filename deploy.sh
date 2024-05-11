a6c91f2bd2c781e7181458854e7b133d2e477553
Neil Song <song.neil.song@gmail.com>
2024-05-10 22:17:17 -0700

    chore: update deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,4 @@ doppler setup -p coursetable -c prod
 (cd traefik && doppler run --command "docker-compose up -d")
 (cd under_maintenance && doppler run --command "docker-compose up -d")
 (cd db && doppler run --command "docker-compose up -d")
+(cd api_services && doppler run --command "docker-compose up -d")
