afb2339d747090a8e04f40f7b005f4e3bdd5245c
Neil Song <song.neil.song@gmail.com>
2024-05-12 22:51:07 -0700

    feat: ensure latest images are pulled on deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 doppler setup -p coursetable -c prod
-(cd traefik && doppler run --command "docker compose up -d")
-(cd under_maintenance && doppler run --command "docker compose up -d")
-(cd db && doppler run --command "docker compose up -d")
-(cd api_services && doppler run --command "docker compose up -d")
+(cd traefik && doppler run --command "docker compose pull && docker compose up -d")
+(cd under_maintenance && doppler run --command "docker compose pull && docker compose up -d")
+(cd db && doppler run --command "docker compose pull && docker compose up -d")
+(cd api_services && doppler run --command "docker compose pull && docker compose up -d")
