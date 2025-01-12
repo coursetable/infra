b69912c88091ae0109f061a6c38e7d82c89e1afd
Neil Song <song.neil.song@gmail.com>
2025-01-12 04:54:04 -0600

    fix: proper syntax

--- db/restore-ferry.sh
+++ db/restore-ferry.sh
@@ -2,9 +2,9 @@
 
 set -euo pipefail
 
-docker exec -i dropdb -U postgres -f ferry
-docker exec -i createdb -U postgres ferry
+docker exec -i db dropdb -U postgres -f ferry
+docker exec -i db createdb -U postgres ferry
 export LATEST_BACKUP=$(ls -Art /home/app/ferry_snapshots  | tail -n 1)
-docker exec -i psql -U postgres -d ferry < /home/app/ferry_snapshots/$LATEST_BACKUP
+docker exec -i db psql -U postgres -d ferry < /home/app/ferry_snapshots/$LATEST_BACKUP
 docker exec -i db psql -U postgres -d ferry < $(pwd)/hasura-user.sql
 docker exec -i db psql -U postgres -d ferry < $(pwd)/ferry-user.sql
