3d32844c5aa25e7e9a0af97bd86a10243afef9a7
Neil Song <song.neil.song@gmail.com>
2025-01-12 04:38:02 -0600

    fix: correct user

--- db/ferry-user.sql
+++ db/ferry-user.sql
@@ -1,7 +1,8 @@
 -- This only has to be done once to initialize the new user.
 -- Run this in the ferry DB.
 
-GRANT ALL ON SCHEMA public TO hasura;
+GRANT ALL ON SCHEMA public TO ferry;
+ALTER SCHEMA public OWNER TO ferry;
 GRANT ALL ON ALL TABLES IN SCHEMA public TO ferry;
 GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO ferry;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO ferry;
