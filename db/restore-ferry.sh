6a9fe265eafff848a39e38a16473d89c8b27563e
Reyansh Bahl <bahl.reyansh@gmail.com>
2026-01-09 23:06:08 -0500

    feat: restore specific ferry snapshot file

--- db/restore-ferry.sh
+++ db/restore-ferry.sh
@@ -2,9 +2,15 @@
 
 set -euo pipefail
 
+if [ $# -gt 0 ]; then
+    BACKUP_TO_RESTORE="/home/app/ferry_snapshots/$1"
+else
+    export LATEST_BACKUP=$(ls -Art /home/app/ferry_snapshots/*.sql | tail -n 1)
+    BACKUP_TO_RESTORE="/home/app/ferry_snapshots/$LATEST_BACKUP"
+fi
+
 docker exec -i db dropdb -U postgres -f ferry
 docker exec -i db createdb -U postgres ferry
-export LATEST_BACKUP=$(ls -Art /home/app/ferry_snapshots  | tail -n 1)
-docker exec -i db psql -U postgres -d ferry < /home/app/ferry_snapshots/$LATEST_BACKUP
+docker exec -i db psql -U postgres -d ferry < "$BACKUP_TO_RESTORE"
 docker exec -i db psql -U postgres -d ferry < $(pwd)/hasura-user.sql
 docker exec -i db psql -U postgres -d ferry < $(pwd)/ferry-user.sql
